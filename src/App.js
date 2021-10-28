18a19de0d20ced47a7528775d5c0327107c84fdb
Chris Ha <chriskevinha@gmail.com>
2021-10-28 03:22:07 -0700

    add auto-lock for iPad

--- src/App.js
+++ src/App.js
@@ -5,12 +5,25 @@ const StyledApp = styled.div`
   min-height: 100vh;
   padding: 4vw;
   background: #111;
+  font-family: 'Ericsson GA628';
+  font-stretch: extra-expanded;
+`;
+const StyledAutolock = styled.a`
+  display: table;
+  margin: 4vw auto 0;
+  font-size: 2vw;
+  text-transform: uppercase;
+  text-decoration: none;
+  color: white;
 `;
 
 function App() {
   return (
     <StyledApp>
       <Clocks />
+      <StyledAutolock href="prefs:root=DISPLAY&path=AUTOLOCK">
+        iPad Auto-Lock 🔒
+      </StyledAutolock>
     </StyledApp>
   );
 }
