bfdb414de723d36926f60ed2155a03176a1b9b2b
Chris Ha <chriskevinha@gmail.com>
2022-04-27 12:54:30 -0700

    remove ipad lock

--- src/App.js
+++ src/App.js
@@ -8,27 +8,11 @@ const StyledApp = styled.div`
   font-family: 'Ericsson GA628';
   font-stretch: extra-expanded;
 `;
-const StyledAutolock = styled.a`
-  display: table;
-  margin: 4vw auto 0;
-  font-size: 2vw;
-  text-transform: uppercase;
-  text-decoration: none;
-  color: white;
-  cursor: pointer;
-`;
 
 function App() {
-  function handleClickAutolock() {
-    window.location.href = 'App-prefs:root=DISPLAY&path=AUTOLOCK';
-  }
-
   return (
     <StyledApp>
       <Clocks />
-      <StyledAutolock onClick={handleClickAutolock}>
-        iPad Auto-Lock 🔒
-      </StyledAutolock>
     </StyledApp>
   );
 }
